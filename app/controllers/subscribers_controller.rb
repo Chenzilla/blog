6dce8ed1747a2959409ee876ea1ea000b68e8cef
chenzilla <alexschen94@gmail.com>
2014-05-20 03:56:11 -0400

    finish subscribe

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -2,4 +2,20 @@ class SubscribersController < ApplicationController
   def new
     @subscriber = Subscriber.new
   end
-end
+
+  def create
+    @subscriber = Subscriber.new(user_params)
+    if @subscriber.save
+      flash[:success] = "Thanks for subscribing!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  private
+
+    def user_params
+      params.require(:subscriber).permit(:email)
+    end
+  end
+
